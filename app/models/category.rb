10157c417ff761d1e949ab121af598be9f3f39a3
renaud dor <renaud.dor@gmail.com>
2020-04-28 19:25:08 +0200

    basic product index

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
-  has_many :products, through: :product_categories
   has_many :product_categories, dependent: :destroy
+  has_many :products, through: :product_categories
 
   has_many :children, class_name: "Category", foreign_key: "parent_id"
   belongs_to :parent, class_name: "Category", optional: true
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   has_many :product_variations, dependent: :destroy
-  has_many :categories, through: :product_categories
   has_many :product_categories, dependent: :destroy
+  has_many :categories, through: :product_categories
 
   validates :name, :price, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0}
