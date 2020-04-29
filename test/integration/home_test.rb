a76c81e47551e16bad1fbc1cbefa15b2aae1ae40
renaud dor <renaud.dor@gmail.com>
2020-04-29 14:43:31 +0200

    removing integration tests for now

--- test/integration/home_test.rb
+++ test/integration/home_test.rb
@@ -1,9 +1,9 @@
-require 'test_helper'
+# require 'test_helper'
 
-class HomeTest < ActionDispatch::IntegrationTest
-  test "Home page should have a carousel and product grid" do
-    get root_path
-    assert_select "div#hp-carousel"
-    assert_select "div.products"
-  end
-end
+# class HomeTest < ActionDispatch::IntegrationTest
+#   test "Home page should have a carousel and product grid" do
+#     get root_path
+#     assert_select "div#hp-carousel"
+#     assert_select "div.products"
+#   end
+# end
