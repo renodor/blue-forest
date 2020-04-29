c576059668ac461b118ace416cace6338b283940
renaud dor <renaud.dor@gmail.com>
2020-04-29 13:06:14 +0200

    solve bug to check if Travis works

--- app/models/product_variation.rb
+++ app/models/product_variation.rb
@@ -1,6 +1,6 @@
 class ProductVariation < ApplicationRecord
   belongs_to :product
 
-  # validates :quantity, numericality: {greater_than_or_equal_to: 0}
+  validates :quantity, numericality: {greater_than_or_equal_to: 0}
   validates :published, presence: true
 end
