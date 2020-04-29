3c7e82acb9ae2489ad223826b1866a89e257893f
renaud dor <renaud.dor@gmail.com>
2020-04-29 19:48:45 +0200

    test failing CI tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ApplicationRecord
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
 
-  validates :name, :price, presence: true
+  # validates :name, :price, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0}
 end
