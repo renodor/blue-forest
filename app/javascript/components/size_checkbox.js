9e31bb73925997a266a2d5a025b4c7f44f35a09a
renaud dor <renaud.dor@gmail.com>
2020-05-06 23:47:58 +0200

    js for enable/disable atc button on the pdp whether a size is selected or not

--- app/javascript/components/size_checkbox.js
+++ app/javascript/components/size_checkbox.js
@@ -3,34 +3,41 @@ const sizeCheckbox = () => {
   if (sizes.length > 0) {
     const atc = document.querySelector('.pdp .atc');
     const atcOverlay = document.querySelector('.pdp .atc-overlay');
+    const sizeWarning = document.querySelector('.size-selection-warning');
 
-    const isOneSizeChecked = () => {
+    // disable add to cart button by default
+    atc.disabled = true
+
+    // check if on size is selected (after making sure the page is fully loaded)
+    // if yes, disable the overlay and enable the add to cart button
+    // this step is needed if you hit the 'previous' button of your browser
+    setTimeout(() => {
       sizes.forEach(size => {
-        console.log(size.checked);
-      });
-    };
+        if(size.checked) {
+          atcOverlay.style.display = 'none';
+          atc.disabled = false
+        }
+      })
+    }, 1);
 
-    isOneSizeChecked();
-    // if (isOneSizeChecked() == false) {
-    //   atc.disabled = true
-    // }
+    // When a size is checked, disable the overlay, disable warning message, and enable the add to cart button
+    sizes.forEach(size => {
+      size.addEventListener('click', event => {
+        if (size.checked) {
+          atcOverlay.style.display = 'none';
+          sizeWarning.style.display = 'none';
+          atc.disabled = false;
+        }
+      })
+    });
 
-
-    // sizes.forEach(size => {
-    //   size.addEventListener('click', event => {
-    //     if (size.checked == true) {
-    //       console.log('yo')
-    //       atcOverlay.style.display = 'none'
-    //       atc.disabled = false;
-    //     }
-    //   })
-    // });
-
-    // atcOverlay.addEventListener('click', event => {
-    //   if (atc.disabled == true) {
-    //     console.log('hey');
-    //   }
-    // })
+    // if add to cart is disabled and there is a click on it show a warning message
+    // (we need to have a add to cart overlay for that because disabled button don't trigger events)
+    atcOverlay.addEventListener('click', event => {
+      if (atc.disabled) {
+        sizeWarning.style.display = 'block';
+      }
+    })
 
   }
 }
