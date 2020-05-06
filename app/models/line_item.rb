5370296ce99ec80658367d57ecb98bca1d49c3c0
renaud dor <renaud.dor@gmail.com>
2020-05-06 11:23:12 +0200

    start sub-total and total price logic for cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,8 @@ class LineItem < ApplicationRecord
   belongs_to :product_variation
   belongs_to :cart
   belongs_to :order, optional: true
+
+  def total_price
+    self.quantity * self.product_variation.price
+  end
 end
