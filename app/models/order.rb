8a7b8caee66788e44d37df9fbaf0334f14b5e7c0
renaud dor <renaud.dor@gmail.com>
2020-05-07 10:49:35 +0200

    order confirmation

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,13 @@ class Order < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :fake_user, optional: true
   has_many :line_items, dependent: :destroy
+  has_many :product_variations, through: :line_items
+
+  def sub_total
+    sum = 0
+    self.line_items.each do |line_item|
+      sum += line_item.total_price
+    end
+    sum
+  end
 end
