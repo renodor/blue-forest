21a67238b886f0d7c76b2b10792e9f9aa96711be
renaud dor <renaud.dor@gmail.com>
2020-05-08 10:36:31 +0200

    remove address from users in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -77,7 +77,7 @@ p '----------> done'
 p 'Create users'
 
 10.times do |n|
-  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone: Faker::PhoneNumber.cell_phone, address: Faker::Address.street_address, password: '123456', password_confirmation: '123456', email: "user#{n + 1}@gmail.com")
+  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone: Faker::PhoneNumber.cell_phone, password: '123456', password_confirmation: '123456', email: "user#{n + 1}@gmail.com")
   p "user #{n + 1} created!"
 end
 
