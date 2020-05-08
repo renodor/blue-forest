72f65624b19e202be03290d79e6382077938fb62
renaud dor <renaud.dor@gmail.com>
2020-05-08 16:53:48 +0200

    add validations to address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :fake_user, optional: true
+
+  validates :street, :district, :detail, :city, :latitude, :longitude, presence: true
 end
