782f0a9f81ffda4a03416b2b0715fcde875b5b76
renaud dor <renaud.dor@gmail.com>
2020-05-10 17:03:14 +0200

    start cart controller tests

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -1,5 +1,16 @@
 require 'test_helper'
 
 class CartsControllerTest < ActionDispatch::IntegrationTest
+  # include Devise::Test::IntegrationHelpers
 
+  # def setup
+  #   @cart = carts(:cart1)
+  # end
+
+  # test 'should get show' do
+  #   get root_path
+  #   p session[:cart_id]
+  #   get cart_path(session[:cart_id])
+  #   assert_response :success
+  # end
 end
