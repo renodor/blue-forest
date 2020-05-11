c1636f07c466bffdf5f74ae11c9dbd6908892fff
renaud dor <renaud.dor@gmail.com>
2020-05-11 11:26:21 +0200

    configure order confirmation email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,7 @@
 class OrderMailer < ApplicationMailer
   def confirmation(order)
     @order = order
-    @order.fake_user_id ? @user = @order.fake_user : @user = @order.user
+    @user = @order.fake_user || @order.user
     mail(to: @user.email, subject: 'Confirmación de su pedido en Blueforestpanama.com')
   end
 end
