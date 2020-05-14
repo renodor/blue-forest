eac9f57ad4b571485d27b4d63615a522b04b0bdf
renaud dor <renaud.dor@gmail.com>
2020-05-14 16:36:24 +0200

    dynamically change product price when product variation is selected

--- app/javascript/components/pdp_price.js
+++ app/javascript/components/pdp_price.js
@@ -1,6 +1,17 @@
 const pdpPrice = () => {
 
+  const sizes = document.querySelectorAll('.pdp .size input');
+  const price = document.querySelector('.pdp .price');
 
+
+    sizes.forEach(size => {
+      size.addEventListener('click', event => {
+        if (size.checked) {
+          const new_price = event.currentTarget.dataset.price;
+          price.innerHTML = `$${new_price}`;
+        }
+      })
+    });
 }
 
 
