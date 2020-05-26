09da073185c3f6f9ccc376390f848d2f169651e4
renaud dor <renaud.dor@gmail.com>
2020-05-26 22:09:14 +0200

    add deeplinks to user dashboard tabs

---
+++ app/javascript/components/boostrap_tabs.js
@@ -0,0 +1,36 @@
+// JS to add deeplinks to user dashboard boostrap tabs
+// to send people directly to profile or orders tab
+// source: https://webdesign.tutsplus.com/tutorials/how-to-add-deep-linking-to-the-bootstrap-4-tabs-component--cms-31180
+const boostrapTabs = () => {
+  const dashboard = document.getElementById('dashboardTab');
+
+  if (dashboard) {
+    $(document).ready(() => {
+      let url = location.href.replace(/\/$/, "");
+
+      if (location.hash) {
+        const hash = url.split("#");
+        $('#dashboardTab a[href="#'+hash[1]+'"]').tab("show");
+        url = location.href.replace(/\/#/, "#");
+        history.replaceState(null, null, url);
+        setTimeout(() => {
+          $(window).scrollTop(0);
+        }, 400);
+      }
+
+      $('a[data-toggle="tab"]').on("click", function() {
+        let newUrl;
+        const hash = $(this).attr("href");
+        if(hash == "#profile") {
+          newUrl = url.split("#")[0];
+        } else {
+          newUrl = url.split("#")[0] + hash;
+        }
+        newUrl += "/";
+        history.replaceState(null, null, newUrl);
+      });
+    });
+  }
+}
+
+export { boostrapTabs };
