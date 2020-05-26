dc4d761d0874763eb7b2dd82a5cca512f75cb196
renaud dor <renaud.dor@gmail.com>
2020-05-26 22:38:26 +0200

    improve edit address feature from dashboard

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -86,4 +86,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
+
+    def after_update_path_for(resource)
+      dashboards_path(resource)
+    end
 end
