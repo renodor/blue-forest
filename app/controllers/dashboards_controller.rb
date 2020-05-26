537280ac7cda958462023fe42d8bc6c30227a909
renaud dor <renaud.dor@gmail.com>
2020-05-26 20:26:58 +0200

    finish to add orders to user dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def dashboard
     @address = current_user.addresses.first
-    @orders = current_user.orders
+    @orders = current_user.orders.reverse
   end
 end
