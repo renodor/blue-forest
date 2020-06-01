fa65243596a15e04c046e783cf8af2e0e90daf66
renaud dor <renaud.dor@gmail.com>
2020-06-01 23:16:56 +0200

    solve bugs in product creation tool js

--- app/javascript/components/product_creation.js
+++ app/javascript/components/product_creation.js
@@ -43,36 +43,6 @@ const productCreation = () => {
 
     // add more product photos
     // DRY
-    const addColorBtn = document.getElementById('add-color');
-    addColorBtn.addEventListener('click', event => {
-      colorVariationId += 1
-      const lastColor = document.querySelector('.color-variation');
-      lastColor.insertAdjacentHTML('afterend', `
-        <div class="color-variation" data-id='${colorVariationId}'>
-          ${lastColor.innerHTML}
-        </div>
-      `);
-      addSizeToColorBtns = document.querySelectorAll('.add-size-to-color');
-      addSizeToColorBtns[addSizeToColorBtns.length - 1].dataset.id = colorVariationId;
-    })
-
-    // DRY
-    let addSizeToColorBtns = document.querySelectorAll('.add-size-to-color');
-
-    addSizeToColorBtns.forEach((addSizeToColorBtn) => {
-      addSizeToColorBtn.addEventListener('click', event => {
-        console.log('yo')
-        const lastVariation = document.querySelector('.color-variation .size-variation');
-        lastVariation.insertAdjacentHTML('afterend', `
-          <div class="row size-variation">
-            ${lastVariation.innerHTML}
-          </div>
-        `);
-      })
-    })
-
-    // add more product photos
-    // DRY
     const addPhotosBtn = document.getElementById('add-photos');
     addPhotosBtn.addEventListener('click', event => {
       const lastPhoto = document.querySelector('.product-photo');
@@ -82,6 +52,23 @@ const productCreation = () => {
         </div>
       `);
     })
+
+    const colorVariation = document.querySelector('.color-variation');
+
+    // DRY
+    let addSizeToColorBtns = document.querySelectorAll('.add-size-to-color');
+
+    addSizeToColorBtns.forEach((addSizeToColorBtn) => {
+      addSizeToColorBtn.addEventListener('click', event => {
+        const lastVariation = document.querySelector('.color-variation .size-variation');
+        lastVariation.insertAdjacentHTML('afterend', `
+          <div class="row size-variation">
+            ${lastVariation.innerHTML}
+          </div>
+        `);
+      })
+    })
+
 
     // add more product photos
     // DRY
@@ -96,6 +83,49 @@ const productCreation = () => {
       `);
     })
 
+
+    // DRY
+    // TO CLEAN !! HARDCORE
+    const addColorBtn = document.getElementById('add-color');
+    let colorId = 1
+    addColorBtn.addEventListener('click', event => {
+      let lastColor = document.querySelector(`.color-variation[data-id='${colorId}'`);
+      const oneSizeVariation = lastColor.querySelectorAll('.size-variation')[0].innerHTML;
+      const onePhoto = lastColor.querySelectorAll('.product-photo')[0].innerHTML;
+      colorId += 1;
+      lastColor.insertAdjacentHTML('afterend', `
+        <div class="color-variation" data-id='${colorId}'>
+          ${lastColor.innerHTML}
+        </div>
+      `);
+      lastColor = document.querySelector(`.color-variation[data-id='${colorId}'`);
+      lastColor.querySelector('.size-variations').innerHTML = `<div class="row size-variation">${oneSizeVariation}`;
+
+      lastColor = document.querySelector(`.color-variation[data-id='${colorId}'`);
+      lastColor.querySelector('.product-photos').innerHTML = `<div class="row product-photo">${onePhoto}`;
+
+      addSizeToColorBtns = lastColor.querySelector('.add-size-to-color');
+      addSizeToColorBtns.addEventListener('click', event => {
+        const targetColorSizeVariations = event.currentTarget.parentNode.querySelectorAll('.size-variation');
+        const lastVariation = targetColorSizeVariations[targetColorSizeVariations.length - 1]
+         lastVariation.insertAdjacentHTML('afterend', `
+            <div class="row size-variation">
+              ${lastVariation.innerHTML}
+            </div>
+          `);
+      })
+
+      const addPhotoToColorBtns = lastColor.querySelector('#add-photos-to-color');
+      addPhotoToColorBtns.addEventListener('click', event => {
+        const targetColorProductPhotos = event.currentTarget.parentNode.querySelectorAll('.product-photo');
+        const lastPhoto = targetColorProductPhotos[targetColorProductPhotos.length - 1]
+         lastPhoto.insertAdjacentHTML('afterend', `
+            <div class="row product-photo">
+              ${lastPhoto.innerHTML}
+            </div>
+          `);
+      })
+    })
   }
 }
 
