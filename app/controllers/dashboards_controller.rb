280c80593f03af8929fad26ca47d4c2e225b4992
renaud dor <renaud.dor@gmail.com>
2020-06-01 13:17:06 +0200

    start designing product creation tool

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,4 +3,20 @@ class DashboardsController < ApplicationController
     @address = current_user.addresses.first
     @orders = current_user.orders.reverse
   end
+
+  def product_creation_new
+    if !current_user.admin
+      flash[:alert] = 'Need admin rights to create products'
+      redirect_to root_path
+      return
+    end
+
+    @product = Product.new
+    @product_variation = ProductVariation.new
+    @photo = ProductPhoto.new
+  end
+
+  def product_creation_create
+    raise
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,26 +5,36 @@ Rails.application.routes.draw do
     resources :orders, only: [:index, :show, :new, :create]
     resources :addresses, only: [:new, :create, :edit, :update]
   end
+
   resources :carts, only: [:show, :destroy]
   resources :products do
     collection do
       get :search
     end
   end
+
+  resources :product_variations
+
   resources :line_items, only: [:create, :destroy] do
     member do
       post :add_quantity
       post :reduce_quantity
     end
   end
+
   resources :fake_users, only: [:new, :create, :edit, :update] do
     resources :orders, only: [:index, :show, :new, :create]
     resources :addresses, only: [:new, :create, :edit, :update]
   end
+
   # custom route to redirect user to the correct page when the start an order and want to login after
   get 'login_before_new_order', to: 'orders#login_before_new'
 
   # route to the use profile page
   get 'dashboards', to: 'dashboards#dashboard'
+
+  # route for product creation tool
+  get 'product_creation', to: 'dashboards#product_creation_new'
+  post 'product_creation', to: 'dashboards#product_creation_create'
   root to: 'products#index'
 end
