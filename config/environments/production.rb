05955926b5a47291a4369fd736265a10039b6c93
renaud dor <renaud.dor@gmail.com>
2020-06-02 13:14:02 +0200

    configure action mailer asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,6 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'http://www.blueforestpanama.com'
   config.action_mailer.asset_host = 'http://www.blueforestpanama.com'
 
   # Specifies the header that your server uses for sending files.
