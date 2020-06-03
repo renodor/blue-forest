478118b09f25049a4cbc0fc5ae63344621f9f788
renaud dor <renaud.dor@gmail.com>
2020-06-03 14:51:12 +0200

    finish product creation tool optimization

--- app/javascript/components/product_creation.js
+++ app/javascript/components/product_creation.js
@@ -28,15 +28,23 @@ const productCreation = () => {
     });
 
 
-    const insertNewElement = (lastChildElement, elementType, id=null) => {
+    // method to insert a new element on the DOM regarding its type and its ID
+    const insertNewElement = (elementType, id) => {
+      // retrieve the current color variation (the color variation with id 0 is actually a product without color variations)
+      const currentColor = document.querySelector(`.color-element[data-id='${id}'`);
       let elementToAdd = eval(`${elementType}Element`);
+      let lastChildElement;
 
-      if (id) {
+      if (elementType === 'color') {
+        lastChildElement = document.querySelector('.color-variations .color-element:last-child')
         elementToAdd = elementToAdd.replace(/(data-id=['"])(\d+)(['"])/g, `$1${id}$3`);
+      } else {
+        lastChildElement = currentColor.querySelector(`.${elementType}-element:last-child`);
       }
 
+
       lastChildElement.insertAdjacentHTML('afterend', `
-        <div class="${elementType}-element" ${id ? `data-id='${id}'` : ''}>
+        <div class="${elementType}-element" data-id='${id}'>
           ${elementToAdd}
         </div>
       `);
@@ -45,9 +53,20 @@ const productCreation = () => {
     const addClickListenerToBtn = (btn) => {
       btn.addEventListener('click', event => {
         const elementType = event.currentTarget.dataset.type;
-        const currentColor = document.querySelector(`.color-element[data-id='${event.currentTarget.dataset.id}'`);
-        const lastChildElement = currentColor.querySelector(`.${elementType}-element:last-child`);
-        insertNewElement(lastChildElement, elementType);
+        const elementId = event.currentTarget.dataset.id;
+
+        if (elementType === 'color') {
+          colorVariationId += 1
+          insertNewElement(elementType, colorVariationId);
+
+          const newColorAddElementBtns = document.querySelectorAll(`.add-element[data-id='${colorVariationId}']`);
+          newColorAddElementBtns.forEach((newColorAddElementBtn) => {
+            addClickListenerToBtn(newColorAddElementBtn);
+          });
+
+        } else {
+          insertNewElement(elementType, elementId);
+        }
       })
     }
 
@@ -55,25 +74,12 @@ const productCreation = () => {
     const photoElement = document.querySelector('.photo-element').innerHTML;
     const colorElement = document.querySelector(".color-element[data-id='1']").innerHTML;
 
+    let colorVariationId = 1
+
     // add more size variations
     const addElementBtns = document.querySelectorAll('.add-element');
     addElementBtns.forEach((addElementBtn) => {
       addClickListenerToBtn(addElementBtn);
-    })
-
-    let colorVariationId = 1
-
-    const addColorBtn = document.querySelector('.add-color');
-    addColorBtn.addEventListener('click', event => {
-      let lastColorVariation = document.querySelector(`.color-element[data-id='${colorVariationId}']`);
-      colorVariationId += 1
-      insertNewElement(lastColorVariation, event.currentTarget.dataset.type, colorVariationId)
-
-      const newColorAddElementBtns = document.querySelectorAll(`.add-element[data-id='${colorVariationId}']`);
-      console.log(newColorAddElementBtns)
-      newColorAddElementBtns.forEach((newColorAddElementBtn) => {
-        addClickListenerToBtn(newColorAddElementBtn);
-      });
     })
   }
 }
