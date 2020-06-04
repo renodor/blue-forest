5d2fc01c39c136911f6d65154083e696ba3c7b3c
renaud dor <renaud.dor@gmail.com>
2020-06-04 15:11:27 +0200

    solve mapbox issues (by adding a timeout...)

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -4,35 +4,45 @@ const initMapbox = () => {
   const createAddressMap = document.getElementById('create-address-map');
   const showAddressMap = document.getElementById('show-address-map');
 
+
   // Interactive map when creating addresses
   if (createAddressMap) {
-    mapboxgl.accessToken = createAddressMap.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
-      container: 'create-address-map',
-      center: [-79.5254181, 9.0152974], // by default showing Panama
-      zoom: 10,
-      style: 'mapbox://styles/mapbox/streets-v10'
-    });
-
-    // Get latitude and longitude hidden inputs
-    const latitudeInput = document.getElementById('address_latitude');
-    const longitudeInput = document.getElementById('address_longitude');
-
-    // Create a new draggable market on the map, and put it by default in Panama
-    const marker = new mapboxgl.Marker({
-      draggable: true
-    })
-    .setLngLat([-79.5254181, 9.0152974])
-    .addTo(map);
-
-    // Method that update the value of latitude and longitude hidden inputs
-    const updateCoordinates = () => {
-      latitudeInput.value = marker.getLngLat().lat;
-      longitudeInput.value = marker.getLngLat().lng;
+    let coordinates;
+    if (createAddressMap.dataset.lng && createAddressMap.dataset.lat) {
+      coordinates = [createAddressMap.dataset.lng, createAddressMap.dataset.lat]
+    } else {
+      coordinates = [-79.5254181, 9.0152974]
     }
 
-    // Every time the marker is dragged, call updateCoordinates method
-    marker.on('dragend', updateCoordinates);
+    setTimeout(() => {
+      mapboxgl.accessToken = createAddressMap.dataset.mapboxApiKey;
+      const map = new mapboxgl.Map({
+        container: 'create-address-map',
+        center: coordinates, // by default showing Panama
+        zoom: 10,
+        style: 'mapbox://styles/mapbox/streets-v10'
+      });
+
+      // Get latitude and longitude hidden inputs
+      const latitudeInput = document.getElementById('address_latitude');
+      const longitudeInput = document.getElementById('address_longitude');
+
+      // Create a new draggable market on the map, and put it by default in Panama
+      const marker = new mapboxgl.Marker({
+        draggable: true
+      })
+      .setLngLat(coordinates)
+      .addTo(map);
+
+      // Method that update the value of latitude and longitude hidden inputs
+      const updateCoordinates = () => {
+        latitudeInput.value = marker.getLngLat().lat;
+        longitudeInput.value = marker.getLngLat().lng;
+      }
+
+      // Every time the marker is dragged, call updateCoordinates method
+      marker.on('dragend', updateCoordinates);
+    }, 500);
   };
 
   // Static map when showing addresse
