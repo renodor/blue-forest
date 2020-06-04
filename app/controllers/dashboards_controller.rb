d915560d5b54b659da3a4a1c64a7a8c08e81b6f5
renaud dor <renaud.dor@gmail.com>
2020-06-04 17:01:26 +0200

    add form validation for product creation tool

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -22,64 +22,75 @@ class DashboardsController < ApplicationController
   end
 
   def product_creation_create
-    # create a new product with product creation form params
-    @product = Product.new(name: params[:name], short_description: params[:short_description], long_description: params[:long_description])
-    params[:published] ? @product.published = true : @product.published = false
-    @product.save!
+    if form_validation[:valid?]
+      # create a new product with product creation form params
+      @product = Product.new(name: params[:name], short_description: params[:short_description], long_description: params[:long_description])
+      params[:published] ? @product.published = true : @product.published = false
+      @product.save!
 
-    # iterate over all color variations params
-    params[:color_variations].each do |color_variation|
-      # both product 'without colors' and 'with colors' options are present on the form DOM
-      # it means even if we create a product without colors, the form will also have (empty) params for a product variation with colors
-      # (and the contrary as well : if we create a product with colors, the form will also have (empty) params for a product variation without colors)
-      # but product variation without color color value is 'unique' by default'
-      # So if product type is 'without colors', it means we can skip product variations that dont have the color value 'unique'
-      # and if product type is 'with colors', it means we can skip product variations that has the color value 'unique'
-      next if params[:product_type] == 'without_colors' && color_variation[:color] != 'unique'
-      next if params[:product_type] == 'with_colors' && color_variation[:color] == 'unique'
+      # iterate over all color variations params
+      params[:color_variations].each do |color_variation|
+        # both product 'without colors' and 'with colors' options are present on the form DOM
+        # it means even if we create a product without colors, the form will also have (empty) params for a product variation with colors
+        # (and the contrary as well : if we create a product with colors, the form will also have (empty) params for a product variation without colors)
+        # but product variation without color color value is 'unique' by default'
+        # So if product type is 'without colors', it means we can skip product variations that dont have the color value 'unique'
+        # and if product type is 'with colors', it means we can skip product variations that has the color value 'unique'
+        next if params[:product_type] == 'without_colors' && color_variation[:color] != 'unique'
+        next if params[:product_type] == 'with_colors' && color_variation[:color] == 'unique'
 
-      # iterate over size variations params
-      color_variation[:size_variations].each do |size_variation|
-        # for each size variation, create a new Product Variation instance with size variation params
-        product_variation = ProductVariation.new(product_id: @product.id, size: size_variation[:size], price: size_variation[:price], discount_price: size_variation[:discount_price], quantity: size_variation[:quantity])
-        # product variation color need to stay empty if there is no colors
-        # so put the color variation params unless the color value is 'unique'
-        product_variation.color = color_variation[:color] unless color_variation[:color] == 'unique'
-        product_variation.save!
-      end
-
-      # initialize a photo array
-      photos = []
-
-      # create a new instance of product photos and attach it to the product
-      @product_photo = ProductPhoto.new
-      @product_photo.product = @product
-
-      # iterate over photos params
-      color_variation[:photos].each do |variation_photo|
-        if variation_photo[:main]
-          # if this is the main photo, put the photo at the beginning of the photos array
-          # and set this product photo as the main one
-          photos.unshift({io: variation_photo[:photo], filename: @product.name, content_type: variation_photo[:photo].content_type})
-          @product_photo.main = true
-        else
-          # otherwise just add photo to the photos array
-          photos << {io: variation_photo[:photo], filename: @product.name, content_type: variation_photo[:photo].content_type}
+        # iterate over size variations params
+        color_variation[:size_variations].each do |size_variation|
+          # for each size variation, create a new Product Variation instance with size variation params
+          product_variation = ProductVariation.new(product_id: @product.id, size: size_variation[:size], price: size_variation[:price], discount_price: size_variation[:discount_price], quantity: size_variation[:quantity])
+          # product variation color need to stay empty if there is no colors
+          # so put the color variation params unless the color value is 'unique'
+          product_variation.color = color_variation[:color] unless color_variation[:color] == 'unique'
+          product_variation.save!
         end
+
+        # initialize a photo array
+        photos = []
+
+        # create a new instance of product photos and attach it to the product
+        @product_photo = ProductPhoto.new
+        @product_photo.product = @product
+
+        # iterate over photos params
+        color_variation[:photos].each do |variation_photo|
+          if variation_photo[:main]
+            # if this is the main photo, put the photo at the beginning of the photos array
+            # and set this product photo as the main one
+            photos.unshift({io: variation_photo[:photo], filename: @product.name, content_type: variation_photo[:photo].content_type})
+            @product_photo.main = true
+          else
+            # otherwise just add photo to the photos array
+            photos << {io: variation_photo[:photo], filename: @product.name, content_type: variation_photo[:photo].content_type}
+          end
+        end
+
+        # attach all photos to this product photo instance
+        @product_photo.photos.attach(photos)
+        # product photo color need to stay empty if there is no colors
+        # so put the color variation params unless the color value is 'unique'
+        @product_photo.color = color_variation[:color] unless color_variation[:color] == 'unique'
+        @product_photo.save!
       end
-
-      # attach all photos to this product photo instance
-      @product_photo.photos.attach(photos)
-      # product photo color need to stay empty if there is no colors
-      # so put the color variation params unless the color value is 'unique'
-      @product_photo.color = color_variation[:color] unless color_variation[:color] == 'unique'
-      @product_photo.save!
+      flash[:notice] = "Product Created"
+      redirect_to product_creation_path
+    else
+      flash[:alert] = form_validation[:error_message]
+      render :product_creation_new
     end
-
-    flash[:notice] = "Product Created"
-    redirect_to product_creation_path
   end
 
-  private
+  def form_validation
+    result = { valid?: false, error_message: '' }
+
+    result[:error_message] = 'Product name is empty' if params[:name].empty?
+    raise
+
+    return result
+  end
 
 end
