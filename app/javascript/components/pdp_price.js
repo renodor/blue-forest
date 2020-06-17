f032cecdd9e8b01c0cd9fc0a3bc65da1f1a8fa62
renaud dor <renaud.dor@gmail.com>
2020-06-17 10:30:51 +0200

    solve pdp price issue for unique sizes

--- app/javascript/components/pdp_price.js
+++ app/javascript/components/pdp_price.js
@@ -3,26 +3,45 @@ const pdpPrice = () => {
   const pdpContainer = document.querySelector('.pdp-container');
   if (pdpContainer) {
     const sizes = document.querySelectorAll('.pdp .sizes input');
+    const colors = document.querySelectorAll('.pdp .colors input');
     const price = document.querySelector('.pdp .price');
     const discountPrice = document.querySelector('.pdp .discount-price');
+
+    // helper method to update the price (and discount price) on the PDP
+    // regarding what product variation (size variation) is selected
+    const updatePrice = (productVariation) => {
+      const newPrice = productVariation.dataset.price;
+      price.innerHTML = `$${newPrice}`;
+      if (productVariation.dataset.discountPrice) {
+        const newDiscountPrice = productVariation.dataset.discountPrice;
+        price.classList.add('crossed');
+        discountPrice.innerHTML = `$${newDiscountPrice}`;
+      } else {
+        discountPrice.innerHTML = "";
+        price.classList.remove('crossed');
+      }
+    }
 
     // update price regarding what product variation is selected
     sizes.forEach(size => {
       size.addEventListener('click', event => {
         if (size.checked) {
-          const newPrice = event.currentTarget.dataset.price;
-          price.innerHTML = `$${newPrice}`;
-          if (event.currentTarget.dataset.discountPrice) {
-            const newDiscountPrice = event.currentTarget.dataset.discountPrice;
-            price.classList.add('crossed');
-            discountPrice.innerHTML = `$${newDiscountPrice}`;
-          } else {
-            discountPrice.innerHTML = "";
-            price.classList.remove('crossed');
-          }
+          updatePrice(event.currentTarget);
         }
       })
     });
+
+    // if there are color variations but unique sizes, sizes don't appear on front end, so they can't be clicked
+    // in that case we need to check for clicks on the colors and show the price corresponding to the correct color
+    if (sizes[0].dataset.unique) {
+      colors.forEach(color => {
+        color.addEventListener('click', event => {
+          const targetColor = event.currentTarget.value;
+          const targetSize = document.querySelector(`.pdp .sizes input[data-color=${targetColor}`)
+          updatePrice(targetSize);
+        })
+      })
+    }
   };
 }
 
