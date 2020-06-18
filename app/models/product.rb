bef9d5210f69500e66cc83188f4111f174b986e8
renaud dor <renaud.dor@gmail.com>
2020-06-18 17:48:09 +0200

    add markdown to html feature for product long description

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,26 @@ class Product < ApplicationRecord
 
   validates :name, presence: true
   validates :published, inclusion: { in: [true, false] }
+
+  before_save :convert_long_description_to_html, if: :will_save_change_to_long_description?
+
+  private
+
+  # convert long_description (pseudo) markdown to html
+  def convert_long_description_to_html
+    # convert bold text
+    self.long_description.gsub!(/\*\*(.+)\*\*/, '<strong>\1</strong>')
+
+    # convert unordered lists
+    self.long_description.gsub!(/(\*.+(\r|\n|\r\n))+/) do |list|
+      items = "<ul>\n"
+      list.gsub(/\*.+/) do |li|
+        items << "<li>#{li.sub(/^\*/, '').strip}</li>\n"
+      end
+      items << "</ul>\n"
+    end
+
+    # convert break lines
+    self.long_description.gsub!(/\r\n/, '<br>')
+  end
 end
