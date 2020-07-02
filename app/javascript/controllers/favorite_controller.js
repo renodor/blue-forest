57e7ed9ada06fb5a5edc49ea757e37d4c264889a
renaud dor <renaud.dor@gmail.com>
2020-07-02 22:39:12 +0200

    handle ajax error for favorite feature when user is not connected

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -16,13 +16,14 @@ export default class extends Controller {
       },
       method: 'DELETE'
     }).then(response => {
-      // then we need to remove the favorite-id from the toggle btn
-      // we need to transform the toggle button so that it can now 'add' the product into user favorite list if clicked again
-      // and we need to change the icon
-      this.toggleTarget.dataset.favoriteId = '';
-      this.toggleTarget.dataset.action = 'click->favorite#addFavorite';
-      this.iconTarget.classList.remove('liked');
-    });
+        // when we have the response we need to do 3 things:
+        // - remove the favorite-id from the toggle btn
+        // - transform the toggle button so that it can now 'add' the product into user favorite list if clicked again
+        // - change the icon
+        this.toggleTarget.dataset.favoriteId = '';
+        this.toggleTarget.dataset.action = 'click->favorite#addFavorite';
+        this.iconTarget.classList.remove('liked');
+      })
   }
 
   // when addFavorite toggle btn is clicked
@@ -38,14 +39,22 @@ export default class extends Controller {
       method: 'POST',
       body: JSON.stringify({ product_id: productId})
     }).then(response => response.json())
-      // then we need to add the favorite-id to the toggle btn
-      // we need to transform the toggle button so that it can now 'remove' the product from user favorite list if clicked again
-      // and we need to change the icon
+      // first we need to check if the user is authorized to add product to its favorites (if it is signed in)
+      // if not the response will throw an error, in that case we redirect him to the sign in page
+
+      // if the response doesn't throw an error, we need to do 3 things:
+      // - add the favorite-id to the toggle btn
+      // - transform the toggle button so that it can now 'remove' the product from user favorite list if clicked again
+      // - change the icon
       .then(data => {
-        this.toggleTarget.dataset.favoriteId = data.product_favorite_id;
-        this.toggleTarget.dataset.action = 'click->favorite#removeFavorite';
-        this.iconTarget.classList.add('liked');
-    })
+        if (data.error) {
+          window.location.href = '/users/sign_in';
+        } else {
+          this.toggleTarget.dataset.favoriteId = data.product_favorite_id;
+          this.toggleTarget.dataset.action = 'click->favorite#removeFavorite';
+          this.iconTarget.classList.add('liked');
+        }
+      })
   }
 }
 
