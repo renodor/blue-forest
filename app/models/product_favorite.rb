511fe9fba67b0aba14a0eac3211764c29ff7df58
renaud dor <renaud.dor@gmail.com>
2020-07-02 17:46:27 +0200

    add product favorite uniqueness validation

--- app/models/product_favorite.rb
+++ app/models/product_favorite.rb
@@ -1,4 +1,7 @@
 class ProductFavorite < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  validates :product_id, uniqueness: { scope: :user_id,
+    message: "Este producto ya existe en sus favoritos" }
 end
