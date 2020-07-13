09149b9d40b42e396ff62bff1b8d672076551442
renaud dor <renaud.dor@gmail.com>
2020-07-13 18:58:29 +0200

    solve rubocop offenses on doctors controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,12 +2,12 @@ class DoctorsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:doctor_contact_form]
 
   def doctor_contact_form
-    if contact_params
-      @message = contact_params
-      DoctorMailer.contact(@message).deliver_now
-      flash[:notice] = "Su mensaje se a enviado. Le contactaremos pronto"
-      redirect_to root_path
-    end
+    return unless contact_params
+
+    @message = contact_params
+    DoctorMailer.contact(@message).deliver_now
+    flash[:notice] = 'Su mensaje se a enviado. Le contactaremos pronto'
+    redirect_to root_path
   end
 
   private
