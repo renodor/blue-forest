b97b21542cdc00a7825af67a5a90252af97ad56d
renaud dor <renaud.dor@gmail.com>
2020-07-13 16:00:01 +0200

    solve rubocop offenses on product favorite model

--- app/models/product_favorite.rb
+++ app/models/product_favorite.rb
@@ -2,6 +2,6 @@ class ProductFavorite < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
-  validates :product_id, uniqueness: { scope: :user_id,
-    message: "Este producto ya existe en sus favoritos" }
+  validates :product_id, uniqueness: { scope: :user_id, message:
+    'Este producto ya existe en sus favoritos' }
 end
