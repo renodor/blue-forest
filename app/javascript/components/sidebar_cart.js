09bbb9bdedf78571b967559e421666092527bb98
renaud dor <renaud.dor@gmail.com>
2020-07-22 18:03:25 +0200

    solve eslint offenses on sidebar_cart

--- app/javascript/components/sidebar_cart.js
+++ app/javascript/components/sidebar_cart.js
@@ -3,35 +3,36 @@ const sidebarCart = () => {
   const body = document.querySelector('body');
   const cart = document.getElementById('sidebar-cart');
   const openSidebarCart = document.querySelector('.open-sidebar-cart');
-  const closeSidebarCart = document.querySelector('.close-sidebar-cart')
-  let clickCount = 0
+  const closeSidebarCart = document.querySelector('.close-sidebar-cart');
+  let clickCount = 0;
 
   // helper method that hide the sidebar when clicked anywhere on the body
   // (except on the cart)
   // the clickCount is needed in order to hide it only after the 2nd click
   // (because one 1st click is counted when we click on the cart toggle to open it)
   const bodyClickable = () => {
-    clickCount += 1
+    clickCount += 1;
     if (clickCount > 1) {
       cart.classList.remove('active');
-      clickCount = 0
+      clickCount = 0;
       body.removeEventListener('click', bodyClickable);
     }
-  }
+  };
 
 
   // listen clicks on the cart toggle of the navbar, and open sidebar cart when clicked
   openSidebarCart.addEventListener('click', () => {
     cart.classList.add('active');
-    // when sidebar cart is open, we need to listen click on the body (except on the sidebar cart itself)
+    // when sidebar cart is open, we need to listen click on the body
+    // (except on the sidebar cart itself)
     // so that if the body is clicked, the sidebar cart is closed
     body.addEventListener('click', bodyClickable);
     // stoping propagation on the sidebar cart remove it from the click event listener
     // (so that when you click on the body it close it, but not on the cart itself)
-    cart.addEventListener("click", event => {
+    cart.addEventListener('click', (event) => {
       event.stopPropagation();
     });
-  })
+  });
 
   // listen clicks on the closing sidebar cart button
   closeSidebarCart.addEventListener('click', () => {
@@ -39,8 +40,8 @@ const sidebarCart = () => {
     // and put the clickCount to 0 again (for next time)
     cart.classList.remove('active');
     body.removeEventListener('click', bodyClickable);
-    clickCount = 0
+    clickCount = 0;
   });
-}
+};
 
-export { sidebarCart };
+export {sidebarCart};
