bbfe6d3df3bbf1a3ae34db42cfc556e41bc845c5
renaud dor <renaud.dor@gmail.com>
2020-08-12 16:50:38 +0200

    user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -35,7 +35,25 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test 'user has many orders' do
+  test 'user can have many orders' do
     assert_equal @user.orders.count, 2
+  end
+
+  test 'user can have many product favorites' do
+    assert_equal @user.product_favorites.count, 2
+  end
+
+  test 'when destroy an user, its product favorites should be destroyed' do
+    assert_difference 'ProductFavorite.count', -2 do
+      @user.destroy
+    end
+  end
+
+  test 'user remove_orders_foreign_key before destroy callback' do
+    orders = @user.orders
+    @user.destroy
+    orders.each do |order|
+      assert_nil order.user
+    end
   end
 end
