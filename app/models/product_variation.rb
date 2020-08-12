ed5dd0a684a1d52b2d03f28229216887509ed4b2
renaud dor <renaud.dor@gmail.com>
2020-08-12 16:18:35 +0200

    product variations test model

--- app/models/product_variation.rb
+++ app/models/product_variation.rb
@@ -2,7 +2,7 @@ class ProductVariation < ApplicationRecord
   belongs_to :product, touch: true
   has_many :line_items, dependent: :destroy
 
-  validates :price, :quantity, :size, :name, presence: true
+  validates :price, :quantity, :size, presence: true
   validates :price, :quantity, numericality: { greater_than_or_equal_to: 0 }
   validates :published, inclusion: { in: [true, false] }
 
--- test/models/product_variation_test.rb
+++ test/models/product_variation_test.rb
@@ -15,6 +15,14 @@ class ProductVariationTest < ActiveSupport::TestCase
     assert @variation.published
   end
 
+  test 'product variation should have a published value (true or false)' do
+    @variation.published = false
+    assert @variation.valid?
+
+    @variation.published = nil
+    assert_not @variation.valid?
+  end
+
   test 'product variation should have a quantity' do
     @variation.quantity = nil
     assert_not @variation.valid?
@@ -28,6 +36,14 @@ class ProductVariationTest < ActiveSupport::TestCase
   test 'product variation should have a size' do
     @variation.size = nil
     assert_not @variation.valid?
+  end
+
+  test 'product variation add name callback' do
+    @variation.save
+    assert_match "#{@variation.product.name}-#{@variation.size}", @variation.name
+
+    @variation.update(color: 'red')
+    assert_match "#{@variation.product.name}-#{@variation.size}-#{@variation.color}", @variation.name
   end
 
   test 'product variation price should be a number greater or equal to zero' do
@@ -47,12 +63,6 @@ class ProductVariationTest < ActiveSupport::TestCase
     @variation.quantity = 0
     assert @variation.valid?
   end
-
-  # test 'if quantity == 0, product variation should not be published' do
-  #   @variation.quantity = 0
-  #   @variation.save
-  #   assert_not @variation.published
-  # end
 
   test 'when destroy a product variation, its line items should be destroyed' do
     assert_difference 'LineItem.count', -2 do
