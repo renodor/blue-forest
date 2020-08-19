45d1818e9a0b866d6082828b6ad8bebec9da8dab
renaud dor <renaud.dor@gmail.com>
2020-08-19 14:11:52 +0200

    create fake user controller tests

--- test/controllers/fake_users_controller_test.rb
+++ test/controllers/fake_users_controller_test.rb
@@ -1,12 +1,17 @@
 require 'test_helper'
 
 class FakeUsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @fake_user = fake_users(:fake_user1)
+  end
+
   test 'should get a new' do
     get new_fake_user_path
+    assert_template 'fake_users/new'
     assert_response :success
   end
 
-  test 'create fake user' do
+  test 'create valid fake user' do
     assert_difference 'FakeUser.count', 1 do
       post fake_users_path, params: {
         fake_user: {
@@ -17,5 +22,30 @@ class FakeUsersControllerTest < ActionDispatch::IntegrationTest
         }
       }
     end
+    assert_redirected_to new_fake_user_address_path(FakeUser.last)
+  end
+
+  test "don't create invalid fake user and render new" do
+    assert_no_difference 'FakeUser.count' do
+      post fake_users_path, params: { fake_user: { first_name: '' } }
+    end
+    assert_template 'fake_users/new'
+  end
+
+  test 'edit fake user' do
+    get edit_fake_user_path(@fake_user)
+    assert_template 'fake_users/edit'
+    assert_response :success
+  end
+
+  test 'update valid fake user' do
+    patch fake_user_path(@fake_user), params: {
+      fake_user: {
+        first_name: 'new name'
+      }
+    }
+    @fake_user.reload
+    assert_equal @fake_user.first_name, 'new name'
+    assert_redirected_to new_fake_user_order_path(@fake_user)
   end
 end
