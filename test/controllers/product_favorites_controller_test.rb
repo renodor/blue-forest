b19e1d26abb4d5b6d4f9fa65442290abc685e9cb
renaud dor <renaud.dor@gmail.com>
2020-08-19 17:45:44 +0200

    test product favorites controller create method

--- test/controllers/product_favorites_controller_test.rb
+++ test/controllers/product_favorites_controller_test.rb
@@ -1,26 +1,22 @@
 require 'test_helper'
+require 'json'
 
 class ProductFavoritesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   def setup
     @user = users(:user1)
+    @product = products(:product3)
   end
 
   test 'create product favorite' do
-    post line_items_path, params: {
-      quantity: '1',
-      variation_id: @variation.id
-    }
-
-    @current_cart = LineItem.last.cart
-
-    assert_no_difference 'LineItem.count' do
-      assert_difference 'LineItem.last.quantity', 1 do
-        post add_quantity_line_item_path(LineItem.last.id, quantity: '1'), xhr: true
-      end
+    sign_in(@user)
+    assert_difference 'ProductFavorite.count', 1 do
+      post product_favorites_path(product_id: @product.id), xhr: true
     end
-
+    response = { product_favorite_id: ProductFavorite.last.id }.to_json
     assert_equal 'application/json; charset=utf-8', @response.content_type
-    assert_equal expected_ajax_sucess_response.to_json, @response.body
+    assert_equal response, @response.body
   end
 
   test 'remove product from favorites' do
